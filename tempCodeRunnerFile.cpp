32162ca7a6851518eafb96a913bbec9a0bb823ef
ms267 <dosic38046@padvn.com>
2025-03-01 20:09:53 +0530

    first commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"hello world"<<endl;
+    // int a,b,c;
+    // a=20;
+    // cout<<a<<endl;
+    // string sd = "hello";
+    // cout<< sd;
+    int a,b;
+    cout<<"enter first number: "<<endl;
+    cin>>a;
+    cout<<"enter second number: "<<endl;
+    cin>>b;
+    cout<<"a - b is "<<a - b <<endl;
+    cout<<"a * b is "<<a * b <<endl;
+    cout<<"a / b is "<<(float)a / b <<endl;
+    cout<<"a + b is "<<a + b <<endl;
+
+}
